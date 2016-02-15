7a1683ce03496a0e24f7b499484e96ef97727fd6
akshay1189 <akshay1189@gmail.com>
2016-02-16 01:54:05 +0530

    Implement web services

---
+++ app/src/main/java/com/app/traphoria/lacaldabase/NotificationDataSource.java
@@ -0,0 +1,80 @@
+package com.app.traphoria.lacaldabase;
+
+
+import android.content.Context;
+
+import com.app.traphoria.database.DatabaseHelper;
+import com.app.traphoria.database.DatabaseManager;
+import com.app.traphoria.model.NotificationDurationDTO;
+import com.app.traphoria.model.TripCountryDTO;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class NotificationDataSource {
+
+
+    private Dao<NotificationDurationDTO, String> notificationDurationDao;
+
+    public NotificationDataSource(Context mActivity) {
+        DatabaseManager<DatabaseHelper> manager = new DatabaseManager<DatabaseHelper>();
+        DatabaseHelper db = manager.getHelper(mActivity);
+        try {
+            notificationDurationDao = db.getNotificationDurationDao();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    // 1. ------------------ INSERT ------------------------------------------------
+
+
+    public void insertNotificationDuration(List<NotificationDurationDTO> notificationList) {
+        try {
+            delete();
+            for (NotificationDurationDTO dto : notificationList) {
+                notificationDurationDao.create(dto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // 2. ------------------ Fetch ------------------------------------------------
+
+    public List<NotificationDurationDTO> getNotification() throws Exception {
+
+        return notificationDurationDao.queryForAll();
+
+
+    }
+
+
+    //3.----------------Delete--------------------------
+
+    public void delete() {
+        try {
+
+            notificationDurationDao.delete(notificationDurationDao.queryForAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public NotificationDurationDTO getWhereData(String name){
+        try {
+
+            //notificationDurationDao.queryBuilder().where().eq("name",name).q
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/app/traphoria/model/NotificationDurationDTO.java
@@ -0,0 +1,41 @@
+package com.app.traphoria.model;
+
+
+import com.j256.ormlite.field.DatabaseField;
+
+import java.io.Serializable;
+
+public class NotificationDurationDTO implements Serializable {
+    @DatabaseField
+    private String id;
+
+    @DatabaseField
+    private String name;
+
+    @DatabaseField(generatedId = true)
+    private int localID;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLocalID() {
+        return localID;
+    }
+
+    public void setLocalID(int localID) {
+        this.localID = localID;
+    }
+}
